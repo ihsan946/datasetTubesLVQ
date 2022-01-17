94bff0ae85b9db0e32065127345d3b2addc37f38
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2022-01-17 12:21:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from metode.LVQ import LVQ
+import numpy as np
+
+data = np.loadtxt("data.txt", usecols=[0,1,2,3,4])
+data = np.genfromtxt("data.txt", )
+
+lvq = LVQ(data)
+lvq.learning()
+lvq.printBobot()
+
+# print ("Masukkan data tes (ex. 1 1 0 1):")
+# tes = map(int, raw_input().split())
+# lvq.prediksi(tes)
