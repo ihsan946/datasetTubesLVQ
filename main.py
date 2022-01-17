b3d042a34a3731f767f13ff7aa05a2d99e7e8e6b
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2022-01-17 12:31:44 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from metode.LVQ import LVQ
 import numpy as np
 
 data = np.loadtxt("data.txt", usecols=[0,1,2,3,4])
-data = np.genfromtxt("data.txt", )
 
 lvq = LVQ(data)
 lvq.learning()
 lvq.printBobot()
 
-# print ("Masukkan data tes (ex. 1 1 0 1):")
-# tes = map(int, raw_input().split())
-# lvq.prediksi(tes)
+print "Masukkan data tes (ex. 1 1 0 1):"
+tes = map(int, raw_input().split())
+lvq.prediksi(tes)
